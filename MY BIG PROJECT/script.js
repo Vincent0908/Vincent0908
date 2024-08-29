36250fe0be05b32044d1fe8aa67e2a82c57c16ed
Vincent0908 <vincentzebua0@gmail.com>
2024-08-29 17:12:22 +0700

    Add files via upload

---
+++ MY BIG PROJECT/script.js
@@ -0,0 +1,9 @@
+let namaPertama = document.getElementById('firstName')
+let namaTerakhir = document.getElementById('lastName')
+let email = document.getElementById('email')
+let password = document.getElementById('pasword')
+let passwordKonf = document.getElementById('paswordConfirmation')
+
+function buatAkun() {
+    if (namaPertama === "" )
+}
